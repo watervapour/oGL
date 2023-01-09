ededb107e08b9b7a4ab273b6130c9060cfe713b1
watervapour <watervapour@users.noreply.github.com>
2023-01-09 12:58:24 +1100

    Styling and transforms

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,27 @@
 #include <GLFW\glfw3.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "shader.h"
 
 #include <iostream>
 #include <cmath>
 
-
 float vertices[] = {
     // posistions,          colours,            coords
     -0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,   0.0f, 0.0f, // bottom left
     0.5f, -0.5f, 0.0f,      0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
     -0.5f, 0.5f, 0.0f,      0.0f, 0.0f, 1.0f,   0.0f, 1.0f, // top left
-    0.5f, 0.5f, 0.0f,       1.0f, 0.0f, 0.0f,   1.0f, 1.0f // top right
+    0.5f, 0.5f, 0.0f,       1.0f, 0.0f, 0.0f,   1.0f, 1.0f  // top right
 };
 
 float vertices_second[] = {
     -0.8f, -0.6f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, // top middle
     -0.7f, -0.8f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
-    -0.9f, -0.8f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.5f // bottom left
+    -0.9f, -0.8f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.5f  // bottom left
 };
 
 unsigned int indices[] = {
@@ -28,19 +30,19 @@ unsigned int indices[] = {
     2, 3, 1
 };
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+void framebuffer_size_callback(GLFWwindow *window, int width, int height){
     glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow* window){
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+void processInput(GLFWwindow *window){
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
         glfwSetWindowShouldClose(window, true);
     }
 }
 
-int main(int argc, char** argv){
+int main(int argc, char **argv){
 
-    GLFWwindow* window;
+    GLFWwindow *window;
 
     if (!glfwInit()){
         return -1;
@@ -48,7 +50,6 @@ int main(int argc, char** argv){
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
 
     window = glfwCreateWindow(800, 600, "oGL", NULL, NULL);
     if (window == NULL){
@@ -58,7 +59,7 @@ int main(int argc, char** argv){
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    
+
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
         std::cerr << "Failed to init GLAD.\n";
         return -1;
@@ -66,16 +67,16 @@ int main(int argc, char** argv){
 
     int nrAttributes;
     glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
-    std::cout << "Max # of vertex attribs: "<< nrAttributes << "\n";    
+    std::cout << "Max # of vertex attribs: " << nrAttributes << "\n";
 
     shader shader1("./vertex.vs", "./fragment_1.fs");
     shader shader2("./vertex.vs", "./fragment_2.fs");
 
- // buffers and vertex attributes
+    // buffers and vertex attributes
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
-    
+
     unsigned int VBO;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -85,18 +86,16 @@ int main(int argc, char** argv){
     glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6*sizeof(float)));
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
     glEnableVertexAttribArray(2);
 
-
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
-
 
     // second set of VAO, VBO
     unsigned int VAO2;
@@ -106,9 +105,9 @@ int main(int argc, char** argv){
     glGenBuffers(1, &VBO2);
     glBindBuffer(GL_ARRAY_BUFFER, VBO2);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_second), vertices_second, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6*sizeof(float)));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
     glEnableVertexAttribArray(2);
@@ -128,7 +127,7 @@ int main(int argc, char** argv){
     // load and generate texture
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true);
-    unsigned char* data = stbi_load("../res/container.jpg", &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load("../res/container.jpg", &width, &height, &nrChannels, 0);
     if (data){
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
@@ -145,7 +144,7 @@ int main(int argc, char** argv){
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    
+
     int width2, height2, nrChannels2;
     data = stbi_load("../res/awesomeface.png", &width2, &height2, &nrChannels, 0);
     if (data) {
@@ -159,22 +158,27 @@ int main(int argc, char** argv){
     shader1.use();
     glUniform1i(glGetUniformLocation(shader1.ID, "tex1"), 0);
     shader1.setInt("tex2", 1);
-   
 
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    while(!glfwWindowShouldClose(window)){
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    while (!glfwWindowShouldClose(window)) {
         processInput(window);
-        
+
+        glm::mat4 trans = glm::mat4(1.0f);
+        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
+        trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+
         glClearColor(0.1f, 0.45f, 0.35f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         shader1.use();
         glUniform1i(glGetUniformLocation(shader1.ID, "tex1"), 0);
+        unsigned int transformLoc = glGetUniformLocation(shader1.ID, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
-        glBindVertexArray(VAO);        
+        glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 
@@ -182,6 +186,14 @@ int main(int argc, char** argv){
         float sinValue = (sin(timeValue) / 3.0f) + 0.5f;
         shader2.use();
         shader2.setFloat("cyclic", sinValue);
+        trans = glm::mat4(1.0f);
+        trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+        trans = glm::rotate(trans, -timeValue*4, glm::vec3(0.0, 1.0, 0.0));
+        trans = glm::translate(trans, glm::vec3(0.7+cos(-timeValue/5), 0.7+sin(-timeValue/5), 0.0));
+        
+        
+        unsigned int transformLoc2 = glGetUniformLocation(shader2.ID, "transform");
+        glUniformMatrix4fv(transformLoc2, 1, GL_FALSE, glm::value_ptr(trans));
         glBindVertexArray(VAO2);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 

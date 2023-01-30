475d1f4cb20bef44f270da3846c74fbea5ade797
watervapour <watervapour@users.noreply.github.com>
2023-01-30 21:14:09 +1100

    stencils

--- src/main.cpp
+++ src/main.cpp
@@ -104,6 +104,8 @@ int main(int argc, char **argv){
     }
 
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_STENCIL_TEST);
+    glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
 
     int nrAttributes;
     glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
@@ -115,6 +117,7 @@ int main(int argc, char **argv){
     stbi_set_flip_vertically_on_load(true);
     Shader shaderModel("./vertex.vs", "./fragment_model.fs");
     Model pack = Model("../res/backpack/backpack.obj");
+    Shader outlineModel("./vertex.vs", "./fragment_light.fs");
 
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     while (!glfwWindowShouldClose(window)) {
@@ -141,7 +144,7 @@ int main(int argc, char **argv){
 
         processInput(window);
         glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
         glm::mat4 model = glm::mat4(1.0f);
         model = glm::rotate(model, sin(currentFrame / 2), glm::vec3(0.0f, 1.0f, 0.0f));
@@ -149,12 +152,31 @@ int main(int argc, char **argv){
         glm::mat4 view = cam.GetViewMatrix();
         glm::mat4 projection = glm::perspective(glm::radians(cam.fov), 800.0f / 600.0f, 0.1f, 100.0f);
         
+        glStencilFunc(GL_ALWAYS, 1, 0xFF);
+        glStencilMask(0xFF);
         shaderModel.use();
         shaderModel.setMat4("model", model);
         shaderModel.setMat4("view", view);
         shaderModel.setMat4("projection", projection);
 
         pack.Draw(shaderModel);
+
+
+        glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+        glStencilMask(0x00);
+        glDisable(GL_DEPTH_TEST);
+        model = glm::scale(model, glm::vec3(1.1f));
+        outlineModel.use();
+        outlineModel.setMat4("model", model);
+        outlineModel.setMat4("view", view);
+        outlineModel.setMat4("projection", projection);
+        glm::vec3 color = glm::vec3(0.88f, 0.63f, 0.07f);
+        outlineModel.setVec3("color", color);       
+        pack.Draw(outlineModel);
+
+        glStencilMask(0xFF);
+        glStencilFunc(GL_ALWAYS, 1, 0xff);
+        glEnable(GL_DEPTH_TEST);
 
         glfwSwapBuffers(window);
         glfwPollEvents();

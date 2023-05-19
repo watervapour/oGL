126cd722c1ba45d25ad757f2ea8a25d69fc1bb11
watervapour <watervapour@github.com>
2023-05-19 17:27:05 +1000

    Fixed UV issue with exploding model.
    Also uploaded relevant screenshot.

--- src/shaders/explode.fs
+++ src/shaders/explode.fs
@@ -1,9 +1,15 @@
 #version 330 core
 in vec2 TexCoords;
 
-uniform sampler2D texture_diffuse1;
+struct Material{
+	sampler2D texture_diffuse1;
+};
+
+uniform Material material;
+
 out vec4 FragColor;
 
 void main(){
-	FragColor = texture(texture_diffuse1, TexCoords);
+	vec2 newUVs = vec2(TexCoords.x, 1-TexCoords.y);
+	FragColor = texture(material.texture_diffuse1, newUVs);
 }
